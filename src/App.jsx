030a15a22f63266f310ffeac3e0306120d0d5e92
CorinaTalledo <corina.talledo@gmail.com>
2024-06-25 19:37:47 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+function App() {
+  return <></>;
+}
+
+export default App;
